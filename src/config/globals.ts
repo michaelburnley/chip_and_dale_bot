c2a0f4af11bf542610dbf0cd7399bff46bc573f3
Michael Burnley <about@michaelburnley.com>
2022-04-25 08:49:51 -0700

    initialize repo

---
+++ src/config/globals.ts
@@ -0,0 +1,7 @@
+export default {
+    signing_secret: process.env.SLACK_SIGNING_SECRET,
+    bot_token: process.env.SLACK_BOT_TOKEN,
+    app_id: process.env.SLACK_APP_ID,
+    client_id: process.env.SLACK_CLIENT_ID,
+    client_secret: process.env.SLACK_CLIENT_SECRET,
+}
