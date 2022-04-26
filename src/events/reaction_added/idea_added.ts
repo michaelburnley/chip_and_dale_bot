0c7651f11abe4e492838966f8b1c0999fe2aa4c3
Michael Burnley <about@michaelburnley.com>
2022-04-25 20:14:02 -0700

    added base for message moving

---
+++ src/events/reaction_added/idea_added.ts
@@ -0,0 +1,15 @@
+// export default async ({ event, context }) => {
+//     // console.log(event);
+  
+//     if (event.reaction == `bulb`) {
+  
+//       console.log(event);
+//       config.log.info("had an idea");
+//       // @ts-ignore
+//       const item = _.find(channel_list, { id: event.item.channel });
+  
+//       console.log(item)
+  
+//     }
+//     // console.log(channel_list);
+//   }
